e32205b7363c1670bc87b1d6d17f25129a569596
Devapujyula <sdevapujyula@gmail.com>
2023-11-13 20:15:28 +0530

    optimized code

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,32 @@
-import reactImg from './assets/react-core-concepts.png'
 
-const reactDescription = ['Fundamental', 'Crucial', 'Core']
-
-function getRandom(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
+import CoreConcept from './components/CoreConcepts.jsx';
+import  Header  from './components/Header.jsx';
+import { CORE_CONCEPTS } from './data.js';
 
 
-function Welcome() {
-  const description = reactDescription[getRandom(2)]
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} concepts you will need for alomost any app you are going to build
-    </p>
-    </header>
-  )
-}
+
 
 
 function App() {
   return (
     <div>
-    <Welcome />
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+    <Header />
       <main>
+      <section id='core-concepts'>
         <h2>Time to get started!</h2>
+        <ul>
+          <CoreConcept title = {CORE_CONCEPTS[0].title} description = {CORE_CONCEPTS[0].description} image = {CORE_CONCEPTS[0].title}
+           />
+
+{/* passing data to components using props by using spread operator           */}
+          <CoreConcept {...CORE_CONCEPTS[1]} />
+
+          <CoreConcept {...CORE_CONCEPTS[2]} />
+
+          <CoreConcept {...CORE_CONCEPTS[3]} />
+
+        </ul>
+        </section>
       </main>
     </div>
   );
